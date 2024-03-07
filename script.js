d2e562bf9b5bf3ff9f071725c5bdc3bc51c8f6b2
IveryChen <ivy_chen1@brown.edu>
2024-03-06 23:11:40 -0500

    finished writeup

--- script.js
+++ script.js
@@ -34,3 +34,59 @@ document.addEventListener("DOMContentLoaded", function() {
         categoryContainer.appendChild(div);
     });
 });
+
+
+
+const cardsData = [
+    {
+        category: "Art & Museums",
+        imageSrc: "./assets/TerrorVault.jpeg",
+        altText: "Terror Vault",
+        title: "Terror Vault 1874 Spring Haunt",
+        description: "San Francisco’s premiere haunted attraction returns for a brand new season with “The Initiation” an all-new, bigger, and more terrifying theatrical and immersive experience...",
+        time: "08:00am - 11:00pm",
+        price: "Free"
+    },
+    {
+        category: "Eating & Drinking",
+        imageSrc: "./assets/taco.jpeg",
+        altText: "Tacos",
+        title: "“Pay-What-You-Can” Taco Day",
+        description: "Every Friday from 11am to 5 pm, customers can order off the Taco Love menu and pay anywhere from $0 up to $10. When a customer pays the full price of $10, TATO will give a free meal to someone in need in The City.",
+        time: "11:00am - 5:00pm",
+        price: "Free"
+    }
+    // Add more card data as needed
+];
+
+// Function to create a card element
+function createCard(cardData) {
+    const cardContainer = document.getElementById("card-container");
+    const cardDiv = document.createElement("div");
+    cardDiv.classList.add("card");
+
+    const cardContent = `
+        <img src="${cardData.imageSrc}" class="rounded-image" alt="${cardData.altText}">
+        <div class="card-content">
+            <div class="category-frame">
+                <p class="p5">${cardData.category}</p>
+            </div>
+            <p class="p7">${cardData.title}</p>
+            <div class="description">
+                <p class="p8">${cardData.description}</p>
+            </div>
+            <div class="time-price">
+                <p class="p7">${cardData.time}</p>
+                <p class="p9">${cardData.price}</p>
+            </div>
+        </div>
+    `;
+
+    cardDiv.innerHTML = cardContent;
+    cardContainer.appendChild(cardDiv);
+}
+
+// Loop through cardsData and create card elements
+cardsData.forEach(cardData => {
+    createCard(cardData);
+});
